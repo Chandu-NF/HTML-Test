05e42acb9c3831996ce71e041a1c5d30c8e79c2d
Chandu Nadella <Ranjith.Sarikonda@genusplc.com>
2024-09-04 20:26:16 +0530

    Calculator_Commit_1

---
+++ JSCalculator/Calculator.js
@@ -0,0 +1,28 @@
+function dis(val){
+    document.getElementById("result").value += val
+}
+
+function displayFunction(event){
+    if(event.key =="0" || event.key == "1"
+       || event.key =="2" || event.key == "3"
+       || event.key =="4" || event.key == "5"
+       || event.key =="6" || event.key == "7"
+       || event.key =="8" || event.key == "9"
+       || event.key =="+" || event.key == "-"
+       || event.key =="*" || event.key == "/"
+     ){
+        document.getElementById("result").value += event.key
+    }
+}
+
+function clr(){
+    document.getElementById("result").value = " "
+}
+
+let colorsList = ["blue", "red", "yellow", "white"]
+let colorIndex = 0
+
+function changeBgColor(){
+    document.getElementById("myId").style.background = colorsList[colorIndex]
+    colorIndex = (colorIndex + 1) % colorsList.length
+    }
